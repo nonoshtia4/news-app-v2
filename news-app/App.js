eb6e08bc8160613a95f47ac829c41c36864fc77e
nonoshita4 <nonoshita4@gmail.com>
2024-05-21 13:43:31 +0900

    Add itemContainer

--- news-app/App.js
+++ news-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native</Text>
+      <View style={styles.itemContainer}></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  itemContainer: {
+    height: 100,
+    widtth: "100%",
+    backgroundColor: "red",
   },
 });
