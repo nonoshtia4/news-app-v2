470067161608ee24aa218cc396b08f8ba1af81d6
nonoshita4 <nonoshita4@gmail.com>
2024-05-22 20:02:16 +0900

    Add leftContainer and rightContainer

--- news-app/App.js
+++ news-app/App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Image, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}></View>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}></View>
+        <View style={styles.rightContainer}></View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,13 +16,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#eee",
+    alignItems: "center",
+    justifyContent: "center",
   },
   itemContainer: {
     height: 100,
     widtth: "100%",
     backgroundColor: "red",
+    flexDirection: "row",
+  },
+  leftContainer: {
+    width: 100,
+    backgroundColor: "green",
+  },
+  rightContainer: {
+    flex: 1,
+    backgroundColor: "blue",
   },
 });
