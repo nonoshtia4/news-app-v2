781401dca25a0e2e194ab29d90b0b55e149fdb85
nonoshita4 <nonoshita4@gmail.com>
2024-05-25 23:47:45 +0900

    Comverted to props

--- news-app/App.js
+++ news-app/App.js
@@ -5,10 +5,16 @@ import { ListItem } from "./components/ListItem";
 export default function App() {
   return (
     <View style={styles.container}>
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
+      <ListItem
+        imamgeUrl="https://picsum.photos/id/10/300/300"
+        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        author="ReactNews"
+      />
+      <ListItem
+        imamgeUrl="https://picsum.photos/id/20/300/300"
+        title="タイトル"
+        author="Japanニュース"
+      />
       <StatusBar style="auto" />
     </View>
   );
--- news-app/components/ListItem.js
+++ news-app/components/ListItem.js
@@ -1,19 +1,23 @@
 import { StyleSheet, Image, View, Text } from "react-native";
 
-export const ListItem = () => {
-  const sampleText =
-    "Lorem ipsum dolar sit amet, consectetur adipiscing edit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+/**
+ *
+ * @param {
+ *  imageUrl: 画像URL{string}
+ *  title: タイトル{string}
+ *  author: ニュース提供元{string}
+ * } props
+ * @returns
+ */
+export const ListItem = (props) => {
   return (
     <View style={styles.itemContainer}>
       <View style={styles.leftContainer}>
-        <Image
-          style={{ width: 100, height: 100 }}
-          source={{ uri: "https://picsum.photos/id/10/300/300" }}
-        />
+        <Image style={{ width: 100, height: 100 }} source={{ uri: props.imageUrl }} />
       </View>
       <View style={styles.rightContainer}>
-        <Text numberOfLines={3}>{sampleText}</Text>
-        <Text style={styles.subText}>ReactNews</Text>
+        <Text numberOfLines={3}>{props.title}</Text>
+        <Text style={styles.subText}>{props.author}</Text>
       </View>
     </View>
   );
