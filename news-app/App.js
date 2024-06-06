f876547c4b76cedd3ae8977869b1e22036828f50
nonoshita4 <nonoshita4@gmail.com>
2024-06-06 21:49:08 +0900

    Use vector-icons

--- news-app/App.js
+++ news-app/App.js
@@ -4,15 +4,33 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen } from "./screens/HomeScreen";
 import { ArticleScreen } from "./screens/ArticleScreen";
 import { ClipScreen } from "./screens/ClipScreen";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+const screenOptions = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    if (route.name == "HomeTab") {
+      return <FontAwesome name="home" size={size} color={color} />;
+    } else if (route.name === "ClipTab") {
+      return <FontAwesome name="bookmark" size={size} color={color} />;
+    }
+  },
+});
 
 const HomeStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Article" component={ArticleScreen} />
+      <Stack.Screen
+        name="Home"
+        component={HomeStack}
+        options={{ headerShown: false, title: "Home" }}
+      />
+      <Stack.Screen
+        name="Article"
+        component={ArticleScreen}
+        options={{ headerShown: false, title: "Clip" }}
+      />
     </Stack.Navigator>
   );
 };
@@ -20,9 +38,17 @@ const HomeStack = () => {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="HomeTab" component={HomeStack} />
-        <Tab.Screen name="ClipTab" component={ClipScreen} />
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen
+          name="HomeTab"
+          component={HomeStack}
+          options={{ headerShown: false, tabBarLabel: "Home" }}
+        />
+        <Tab.Screen
+          name="ClipTab"
+          component={ClipScreen}
+          options={{ headerShown: false, tabBarLabel: "Clip" }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
