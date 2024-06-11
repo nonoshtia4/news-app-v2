4ff442c856384ef86919ddf7999dc66376833010
nonoshita4 <nonoshita4@gmail.com>
2024-06-11 15:24:48 +0900

    Add useSelector

--- news-app/screens/ArticleScreen.js
+++ news-app/screens/ArticleScreen.js
@@ -1,19 +1,21 @@
 import { StyleSheet, SafeAreaView, Text } from "react-native";
 import { WebView } from "react-native-webview";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ClipButton } from "../components/ClipButton";
 import { addClip } from "../store/userSlice";
 
 export const ArticleScreen = ({ route }) => {
   const { article } = route.params;
   const dispatch = useDispatch();
+  const clips = useSelector((state) => state.user.clips);
+  const isClipped = clips.some((clip) => clip.url === article.url);
 
   const onPressClip = () => {
     dispatch(addClip(article));
   };
   return (
     <SafeAreaView style={styles.container}>
-      <ClipButton onPress={onPressClip} enabled={false} />
+      <ClipButton onPress={onPressClip} enabled={isClipped} />
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
