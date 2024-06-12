bfca0ea7253d20b957eb6d7529b19e5a42dfdde3
nonoshita4 <nonoshita4@gmail.com>
2024-06-12 16:34:15 +0900

    mod useSelector

--- news-app/components/ClipButton.js
+++ news-app/components/ClipButton.js
@@ -1,17 +1,17 @@
 import { StyleSheet, TouchableOpacity } from "react-native";
-import { FontAweSome } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
 export const ClipButton = ({ onPress, enabled }) => {
-    const name = enabled ? "bookmak" : "bookmark-o";
-    return {
-        <TouchableOpacity onPress={onPress} style={styles.container} >
-            <FontAweSome name={name} size={40} color="salmon" />
-        </TouchableOpacity>
-    };
+  const name = enabled ? "bookmak" : "bookmark-o";
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.container}>
+      <FontAwesome name={name} size={40} color="salmon" />
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 5,
-    },
+  container: {
+    padding: 5,
+  },
 });
