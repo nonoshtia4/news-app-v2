6e17f7ec6e2fb763625436919584f859e10a314f
nonoshita4 <nonoshita4@gmail.com>
2024-06-13 14:37:50 +0900

    mod onPressClip

--- news-app/screens/ArticleScreen.js
+++ news-app/screens/ArticleScreen.js
@@ -2,17 +2,21 @@ import { StyleSheet, SafeAreaView, Text } from "react-native";
 import { WebView } from "react-native-webview";
 import { useDispatch, useSelector } from "react-redux";
 import { ClipButton } from "../components/ClipButton";
-import { addClip } from "../store/userSlice";
+import { addClip, deleteClip } from "../store/userSlice";
 
 export const ArticleScreen = ({ route }) => {
   const { article } = route.params;
   const dispatch = useDispatch();
   const clips = useSelector((state) => state.user.clips);
   const isClipped = clips.some((clip) => clip.url === article.url);
-
   const onPressClip = () => {
-    dispatch(addClip(article));
+    if (isClipped) {
+      dispatch(deleteClip(article));
+    } else {
+      dispatch(addClip(article));
+    }
   };
+
   return (
     <SafeAreaView style={styles.container}>
       <ClipButton onPress={onPressClip} enabled={isClipped} />
